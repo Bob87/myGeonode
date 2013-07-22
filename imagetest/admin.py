985d0a95282bb366bf6a3d4a8c05df73f50cff04
Bob <bob@bob-VirtualBox.(none)>
2013-07-23 00:58:59 +0200

    upload images tab only

---
+++ imagetest/admin.py
@@ -0,0 +1,4 @@
+from imagetest.models import Test
+from django.contrib import admin
+
+admin.site.register(Test)
--- imagetest/models.py
+++ imagetest/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    test1 = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.test1
