7ce5bb2d1c668c318ec3c8be5a89d504c1a36e91
Bob <bob@bob-VirtualBox.(none)>
2013-07-26 20:52:49 +0200

    Combobox

---
+++ imagetest/modelOutput.py
@@ -0,0 +1,13 @@
+LAND_INDICATOR = (('RM','Rio Mannu'),
+		  ('TH','Thau'),
+		  ('CH','Chiba'),
+		  ('GA','Gaza'),
+		  ('KO','Kocaeli'),
+		  ('NO','Noce'),
+		  ('NI','Nile'),
+		  ('RD','Riu di Costara'),
+		  ('MA','Maso Maiano'))
+
+HYDROLOGICAL_INDICATOR = ('PRC','ETP','ETR','RUN','SWC','SWE','DIS','FDC','VDF','CLD','LF7','MDF')
+
+
