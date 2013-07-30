4987f5e2d95cdb63567625aaab965d397df1d8f4
Bob <bob@bob-VirtualBox.(none)>
2013-07-30 21:25:06 +0200

    Css wieder da

--- imagetest/modelOutput.py
+++ imagetest/modelOutput.py
@@ -8,6 +8,50 @@ LAND_INDICATOR = (('RM','Rio Mannu'),
 		  ('RD','Riu di Costara'),
 		  ('MA','Maso Maiano'))
 
-HYDROLOGICAL_INDICATOR = ('PRC','ETP','ETR','RUN','SWC','SWE','DIS','FDC','VDF','CLD','LF7','MDF')
+HYDROLOGICAL_INDICATOR = (('PRC','PRC'),
+		('ETP','ETP'),
+		('ETR','ETR'),
+		('RUN','RUN'),
+		('SWC','SWC'),
+		('SWE','SWE'),
+		('DIS','DIS'),
+		('FDC','FDC'),
+		('VDF','VDF'),
+		('CLD','CLD'),
+		('LF7','LF7'),
+		('MDF','MDF'))
+
+TIME_HORIZON = (('REF','1971-2000'),('FUT','2041-2070'))
+
+HYDROLOGICAL_MODEL = (('WAS','WASIM'),
+		('CAT','CATHY'),
+		('TRI','TRIBS'),
+		('GEO','GEOTRANS'),
+		('PRO','PROMET'),
+		('MGR','MGROWA'),
+		('SWA','SWAT'),
+		('MOD','MODFLOW'),
+		('AQU','AQUACROP'),
+		('MIK','MIKE3'),
+		('COD','CODESA'))
+
+CLIMATE_FORCING = (('ERC','ECHAM-5 RCA'),
+		('ERM','ECHAM-5 RACMO'),
+		('ERE','ECHAM-5 REMO'),
+		('HRC','HadCM3 RCA'))
+
+CLIMATE_DATA_PROCESSING = (('1','BDM'),
+		('2','BDB'),
+		('3','BOX'),
+		('4','NOX'),
+		('5','NDX'))
+
+SPATIAL_SCALES = (('1KM','1000m'),
+		('500','500m'),
+		('250','250m'),
+		('100','100m'),
+		('50m','50m'))
+
+
 
 
--- imagetest/views.py
+++ imagetest/views.py
@@ -9,11 +9,11 @@ from imagetest.forms import TestForm
 from django.core.context_processors import csrf
 
 def home(request):
-	form = TestForm()
-	entries = Test.objects.all()
-	args = {}
-	args.update(csrf(request))
-	args['form'] = form
-	return render_to_response('imagetest/upload.html', args)
-
-#	return render_to_response('imagetest/upload.html', RequestContext(request,{'form':form, 'entries': entries,}), args)
+	form = TestForm
+	#entries = Test.objects.all()
+	#args = {}
+	#args.update(csrf(request))
+	#args['form'] = form
+	#return render_to_response('imagetest/upload.html', args)
+	return render_to_response('imagetest/upload.html', RequestContext(request,{'form': form,}))
+	#return render_to_response('imagetest/upload.html', RequestContext(request,{'entries': entries,}))
