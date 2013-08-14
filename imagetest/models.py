bc02b945ff15a15628cc3916b4fd429fba1a214e
Bob <bob@bob-VirtualBox.(none)>
2013-08-14 16:13:07 +0200

    new

--- imagetest/models.py
+++ imagetest/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Test(models.Model):
     csv = models.CharField(max_length=255)
     image = models.ImageField("Image",upload_to='imagetest',blank=True, null=True)
+    cls_path = models.CharField(max_length=200)
     def __unicode__(self):
         return self.csv
 
--- imagetest/views.py
+++ imagetest/views.py
@@ -14,24 +14,44 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from geonode.security.enumerations import AUTHENTICATED_USERS, ANONYMOUS_USERS
 from geonode.security.views import _perms_info
+import os.path
 
 @login_required
 def home(request):
         if request.method == 'POST':
-		form = TestForm(request.POST)
-		if form.is_valid():
-			cd = form.cleaned_data
-			land = cd['land']
-			hydro = cd['hydro']
-			timehorz = cd['timehorz']
-			hydromodel = cd['hydromodel']
-			climateforcing = cd['climateforcing']
-			climatedatapro = cd['climatedatapro']
-			spatialscales = cd['spatialscales']
-			output = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+climateforcing+'_'+climatedatapro+'_'+spatialscales+'.csv'
-			output2 = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+climateforcing+'_'+climatedatapro+'_'+spatialscales+'.png'
-			img = Test.objects.get(csv=output)
-			return render_to_response('imagetest/add.html',{'form': form, 'land':land,'hydro':hydro, 'output': output, 'img':img},context_instance=RequestContext(request))
+		out = {}
+		try:
+			form = TestForm(request.POST)
+			if form.is_valid():
+				cd = form.cleaned_data
+				land = cd['land']
+				hydro = cd['hydro']
+				timehorz = cd['timehorz']
+				hydromodel = cd['hydromodel']
+				climateforcing = cd['climateforcing']
+				climatedatapro = cd['climatedatapro']
+				spatialscales = cd['spatialscales']
+				if (timehorz != 'BOTH') and (climateforcing != 'ALL'):
+					output = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+climateforcing+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					img = Test.objects.get(csv=output)
+					return render_to_response('imagetest/add.html',{'form': form, 'land':land,'hydro':hydro, 'output': output, 'img':img},context_instance=RequestContext(request))
+				elif (timehorz == 'BOTH') and (climateforcing != 'ALL'):
+					output = land+'_'+hydro+'_'+'REF'+'_'+hydromodel+'_'+climateforcing+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					output2 = land+'_'+hydro+'_'+'FUT'+'_'+hydromodel+'_'+climateforcing+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					img2 = Test.objects.get(csv=output)
+					img3 = Test.objects.get(csv=output2)
+					return render_to_response('imagetest/add.html',{'form': form, 'land':land,'hydro':hydro, 'output': output,'output2': output2, 'img2':img2,'img3':img3},context_instance=RequestContext(request))
+				else:
+					output = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+'ERC'+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					output3 = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+'ERM'+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					output4 = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+'ERE'+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					output5 = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+'HRC'+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					output6 = land+'_'+hydro+'_'+timehorz+'_'+hydromodel+'_'+'ALL'+'_'+climatedatapro+'_'+spatialscales+'.csv'
+					img = Test.objects.get(csv=output6)
+					return render_to_response('imagetest/add.html',{'form': form, 'land':land,'hydro':hydro, 'output': output,'output3': output3, 'output4': output4, 'output5': output5, 'output6': output6, 'img':img},context_instance=RequestContext(request))
+		except Exception, e:
+			out['errors'] = str(e)
+			return render_to_response('imagetest/add.html',{'out': out},context_instance=RequestContext(request))
 	else:
 			form=TestForm()
         return render_to_response('imagetest/upload.html',{'form': form}, context_instance=RequestContext(request))
